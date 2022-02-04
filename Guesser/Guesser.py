e7c85ca5fc6e989517e263b46cfca2599eae7cc3
Wen Junhua <wenjh1998@gmail.com>
2022-02-04 10:18:10 -0500

    feat: update guesser

--- Guesser/Guesser.py
+++ Guesser/Guesser.py
@@ -13,6 +13,7 @@ class Guesser(object):
                 letters[index][letter] = letters[index].get(letter, 0) + 1
         self.letters = letters
         self.wrong_list = []
+        self.contains = []
         self.all_wrong_list = []
 
     # Function to calculate the score of the word
@@ -25,25 +26,50 @@ class Guesser(object):
     def get_best_guess(self, template_string: str, previous_guess: str) -> str:
         """Get the best guess"""
         # Get caps string
+        temp_template_string = []
         for index, letter in enumerate(template_string):
             if letter.isupper():
-                self.wrong_position_list.append((index, letter))
+                letter = letter.lower()
+                self.wrong_list.append((index, letter))
+                self.contains.append(letter)
+                temp_template_string.append('_')
+                continue
+
             if letter == '_':
                 self.all_wrong_list.append(previous_guess[index])
-        words = self.word_tree.find_template(template_string)
+            else:
+                self.contains.append(letter)
+            temp_template_string.append(letter)
+            
+        temp_str = ''.join(temp_template_string)
+        words = self.word_tree.find_template(temp_str)
         new_words = []
+
         for word in words:
             is_wrong = False
+
             for letter in self.all_wrong_list:
                 if letter in word:
-                    is_wrong = True
-            if is_wrong:
-                continue
-            for index, letter in self.wrong_list:
-                if word[index] == letter:
                     is_wrong = True
                     break
             if is_wrong:
                 continue
+
+            for letter in self.contains:
+                if letter not in word:
+                    is_wrong = True
+                    break
+            if is_wrong:
+                continue
+
+
+            for index, letter in self.wrong_list:
+                if word[index] == letter:
+                    is_wrong = True
+                    break
+
+            if is_wrong:
+                continue
+
             new_words.append(word)
         return max(new_words, key=self._score_calc)
